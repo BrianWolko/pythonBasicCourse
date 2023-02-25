2d1921f7989ebbc5e2e6ebd967b9df0099c99f28
Brian Wolkomirski <wolkomirski.brian@gmail.com>
2023-02-25 18:16:35 -0300

    calculadora 2 version propia

---
+++ tipos/13-calculadora2.py
@@ -0,0 +1,31 @@
+def suma(n1, n2):
+    return n1 + n2
+
+def resta(n1, n2):
+    return n1 - n2
+
+def multiplicacion(n1, n2):
+    return n1 * n2
+
+def division(n1, n2):
+    return n1 / n2
+
+def programa(op = ""):
+    while op.lower() != "salir":
+        n1 = int(input("Ingrese un número: "))
+        op = input("ingrese la operación a realizar: ")
+        n2 = int(input("ingrese el segundo número: "))
+        match op:
+            case '+':
+                print(suma(n1,n2))
+            case '-':
+                print(resta(n1,n2))
+            case '*':
+                print(multiplicacion(n1,n2))
+            case '/':
+                print(division(n1,n2))
+            case _:
+                print("por favor ingrese dos números y un operador")
+
+programa()
+            
