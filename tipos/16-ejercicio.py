57b7e186f5b73d3d7560c27537b120b8de1808d8
Brian Wolkomirski <wolkomirski.brian@gmail.com>
2023-02-26 12:27:59 -0300

    funcion no_space

--- tipos/16-ejercicio.py
+++ tipos/16-ejercicio.py
@@ -1,9 +1,17 @@
-palabra = "abasba"
+palabra = "ab fg ba"
+
+def no_space(texto):
+    nuevo_texto =""
+    for char in texto:
+        if char != " ":
+            nuevo_texto += char
+    return nuevo_texto
 
 def es_palindromo(texto):
     i = 0
-    j = len(texto)-1
     esPalindromo = True
+    texto = no_space(texto).lower()
+    j = len(texto)-1
     while i < j:
         if texto[i] != texto[j]:
             esPalindromo = False
